7c3a161119274e3962bc0b934eb03e0842e9f694
SteshM <mbithestella0@gmail.com>
2023-06-26 13:13:41 +0300

    Added classes and objects to my application

---
+++ src/main/java/com/example/ConsumeApi/Objects/Geo.java
@@ -0,0 +1,9 @@
+package com.example.ConsumeApi.Objects;
+
+import lombok.Data;
+
+@Data
+public class Geo {
+    private String lat;
+    private String lng;
+}
--- src/main/java/com/example/ConsumeApi/Service/Controller.java
+++ src/main/java/com/example/ConsumeApi/Service/Controller.java
@@ -1,5 +1,8 @@
 package com.example.ConsumeApi.Service;
 
+import com.example.ConsumeApi.Objects.UserResponse;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,18 +10,29 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class Controller {
-    String url = "https://emailvalidation.abstractapi.com/v1/?api_key=e86b841824ec4b7bb9a3ac1aa0ad2463&email=mbithestella0@gmail.com";
+    String url = "https://jsonplaceholder.typicode.com/users/1";
     HttpHeaders httpHeaders ;
     @RequestMapping("/Hello")
-    public ResponseEntity<String> callApi(){
-
+    public UserResponse callApi(HttpServletResponse response1){
+        response1.setContentType("application/json");
         httpHeaders= new HttpHeaders();
         httpHeaders.setContentType(MediaType.APPLICATION_JSON);
         HttpEntity<String> Request = new HttpEntity<>(httpHeaders) ;
 
         RestTemplate restTemplate = new RestTemplate();
         ResponseEntity<String> response;
+        //In the response you pass in the method response.exchange then pass in the url,http method and the httpEntity
         response = restTemplate.exchange(url,HttpMethod.GET,Request,String.class);
-        return response;
+        UserResponse userResponse = new UserResponse();
+//        When using jsonMapper, a try catch is a must
+        JsonMapper jsonMapper = new JsonMapper();
+        try{
+            userResponse = jsonMapper.readValue(response.getBody(), UserResponse.class);
+        }catch (Exception e){
+            System.out.println(e);
+        }
+        userResponse.setName("Stella");
+
+        return userResponse;
     }
 }
