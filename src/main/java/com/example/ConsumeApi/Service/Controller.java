2c132b8372fb549814ac923dda1808283dec40fe
SteshM <mbithestella0@gmail.com>
2023-06-26 16:45:58 +0300

    About fetching details of all users

--- src/main/java/com/example/ConsumeApi/Service/Controller.java
+++ src/main/java/com/example/ConsumeApi/Service/Controller.java
@@ -11,12 +11,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+
 @RestController
 public class Controller {
-    String url = "https://jsonplaceholder.typicode.com/users/1";
+    String url = "https://jsonplaceholder.typicode.com/users";
     HttpHeaders httpHeaders ;
     @RequestMapping("/Hello")
-    public UserResponse callApi(HttpServletResponse response1){
+    public ResponseEntity<String> callApi(HttpServletResponse response1){
         response1.setContentType("application/json");
         httpHeaders= new HttpHeaders();
         httpHeaders.setContentType(MediaType.APPLICATION_JSON);
@@ -26,42 +28,23 @@ public class Controller {
         ResponseEntity<String> response;
         //In the response you pass in the method response.exchange then pass in the url,http method and the httpEntity
         response = restTemplate.exchange(url,HttpMethod.GET,Request,String.class);
-        if(response.getStatusCode()!= HttpStatus.OK){
-            return null;
-        }
-        UserResponse userResponse = new UserResponse();
-//        When using jsonMapper, a try catch is a must
-        JsonMapper jsonMapper = new JsonMapper();
-        try{
-            userResponse = jsonMapper.readValue(response.getBody(), UserResponse.class);
-        }catch (Exception e){
-            System.out.println(e);
-        }
-     userResponse.setName("Stella");
-       userResponse.setUsername("Bella");
-                userResponse.setEmail("mbithestella0@gmail.com");
-
-        Address address = userResponse.getAddress();
-        address.setCity("Nairobi");
-        address.setStreet("Tom Mboya");
-        address.setSuite("Apt.1234");
-        address.setZipcode("00100");
-
-        Geo geo = address.getGeo();
-        geo.setLat("-40");
-        geo.setLng("81");
-
-        userResponse.setPhone("0790692833");
-        userResponse.setWebsite("BossBella");
-
-        Company company = userResponse.getCompany();
-        company.setName("SteshM limited");
-        company.setCatchPhrase("Multi-layered client-server neural-net");
-        company.setBs("harness real-time e-markets");
+//      String[] responses = response.getBody();
+//        ArrayList<UserResponse> userResponses = new ArrayList<>();
+//      for(String one: responses){
+//          UserResponse userResponse = new UserResponse();
+////        When using jsonMapper, a try catch is a must
+//          JsonMapper jsonMapper = new JsonMapper();
+//          try{
+//              userResponse = jsonMapper.readValue(one, UserResponse.class);
+//          }catch (Exception e){
+//              System.out.println(e);
+//          }
+//          userResponses.add(userResponse);
+//      }
 
 
 
 
-        return userResponse;
+        return response;
     }
 }
