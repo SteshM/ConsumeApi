dec98e59eb1e7d19dca46d63a96d6f5969874e25
SteshM <mbithestella0@gmail.com>
2023-06-27 15:31:01 +0300

    added endpoints to fetch all users and a single user by id

--- src/main/java/com/example/ConsumeApi/Service/Controller.java
+++ src/main/java/com/example/ConsumeApi/Service/Controller.java
@@ -7,18 +7,15 @@ import com.example.ConsumeApi.Objects.UserResponse;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.*;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.ArrayList;
 
 @RestController
 public class Controller {
     String url = "https://jsonplaceholder.typicode.com/users";
     HttpHeaders httpHeaders ;
-    @RequestMapping("/Hello")
-    public ResponseEntity<String> callApi(HttpServletResponse response1){
+
+    public ResponseEntity<String> callApi(HttpServletResponse response1, String url){
         response1.setContentType("application/json");
         httpHeaders= new HttpHeaders();
         httpHeaders.setContentType(MediaType.APPLICATION_JSON);
@@ -28,23 +25,28 @@ public class Controller {
         ResponseEntity<String> response;
         //In the response you pass in the method response.exchange then pass in the url,http method and the httpEntity
         response = restTemplate.exchange(url,HttpMethod.GET,Request,String.class);
-//      String[] responses = response.getBody();
-//        ArrayList<UserResponse> userResponses = new ArrayList<>();
-//      for(String one: responses){
-//          UserResponse userResponse = new UserResponse();
+//        if(response.getStatusCode()!= HttpStatus.OK){
+//            return null;
+//        }
+//        UserResponse userResponse = new UserResponse();
 ////        When using jsonMapper, a try catch is a must
-//          JsonMapper jsonMapper = new JsonMapper();
-//          try{
-//              userResponse = jsonMapper.readValue(one, UserResponse.class);
-//          }catch (Exception e){
-//              System.out.println(e);
-//          }
-//          userResponses.add(userResponse);
-//      }
-
-
+//        JsonMapper jsonMapper = new JsonMapper();
+//        try{
+//            userResponse = jsonMapper.readValue(response.getBody(), UserResponse.class);
+//        }catch (Exception e){
+//            System.out.println(e);
+//        }
 
 
         return response;
     }
+    @GetMapping("get1User/{id}")
+    public ResponseEntity<String>Get1User( HttpServletResponse response,@PathVariable("id") long id){
+        return this.callApi(response,url+"/"+id);
+    }
+    @GetMapping("getUsers")
+    public ResponseEntity<String>GetUsers( HttpServletResponse response){
+        return this.callApi(response,url);
+    }
+
 }
